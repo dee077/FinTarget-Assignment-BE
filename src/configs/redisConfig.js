942016075da69452a0d4ee4bb99ca22a3f5fda5f
Deepanshu Sahu <83969256+dee077@users.noreply.github.com>
2024-09-12 19:23:36 +0530

    redis fix

--- src/configs/redisConfig.js
+++ src/configs/redisConfig.js
@@ -1,18 +1,29 @@
-const redis = require('redis');
+const Redis = require('ioredis');
 const { RateLimiterRedis } = require('rate-limiter-flexible');
 require('dotenv').config();
 
-const redisClient = redis.createClient({
+
+const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD, // Optional
 });
 
 const rateLimiter = new RateLimiterRedis({
   storeClient: redisClient,
-  points: 20, // Number of points
+  points: 20, // Number of points or requests
   duration: 60, // Per minute
-  blockDuration: 60, // Block for 1 minute if consumed more than 20 points
+  blockDuration: 1, // Block for 1 minute if consumed more than 20 points
 });
 
-module.exports = rateLimiter;
+redisClient.on('ready', () => {
+  console.log(`Connected to Redis successfully for process ${process.pid}`);
+});
+
+// console.log(redisClient)
+
+redisClient.on('error', (err) => {
+  console.error(`Error on Redis with process ${process.pid}`, err);
+});
+
+
+module.exports = { redisClient, rateLimiter };
